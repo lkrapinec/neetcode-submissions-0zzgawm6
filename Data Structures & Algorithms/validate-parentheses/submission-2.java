077718542ca59eca9016b6f87df1d66dad124238
Leon <lkrapinec@gmail.com>
2026-04-10 08:39:25 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+                continue;
+            }
+            if(stack.isEmpty()){
+                return false;
+            }
+            
+            if(c == ')' && stack.peek() != '('){
+                    return false;
+            }else if(c == '}' && stack.peek() != '{'){
+                    return false;
+            }else if(c == ']' && stack.peek() != '['){
+                    return false;
+            }
+
+            stack.pop();
+        }
+        return stack.isEmpty();
+    }
+}
