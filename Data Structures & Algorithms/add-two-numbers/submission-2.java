6d5eece339aa59fe2a8c9fdde3db91e4eef67af1
Leon <lkrapinec@gmail.com>
2026-05-26 11:53:06 +0200

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            
+            int sum = val1 + val2 + carry;
+            if(sum > 9){
+                sum -= 10;
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+
+            curr.next = new ListNode(sum);
+            curr =curr.next;
+            
+            l1 = l1 == null ? null :l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if(carry == 1){
+            curr.next = new ListNode(1);
+        }
+
+        return dummy.next;
+
+
+        // ListNode head = null;
+
+        // ListNode prev = null;
+        // ListNode next = null;
+        // while(l1 != null || l2 != null){
+        //     int sum;
+        //     if(l1 == null){
+        //         sum = l2.val;
+        //     }else if(l2 == null){
+        //         sum = l1.val;
+        //     }else{
+        //         sum = l1.val + l2.val;
+        //     }
+
+        //     ListNode curr = new ListNode();
+        //     if(next != null){
+        //         sum += next.val;
+        //     }
+
+        //     if(sum > 9){
+        //         sum -= 10;
+        //         next = new ListNode(1);
+        //         curr.next = next;
+
+        //     }else{
+        //         next = null;
+        //     }
+
+        //     curr.val = sum;
+        //     if(prev == null){
+        //         head = curr;
+        //     }else{
+        //         prev.next = curr;
+        //     }
+
+        //     prev = curr;
+        //     if(l1 != null){
+        //         l1 = l1.next;
+        //     }
+        //     if(l2 != null){
+        //         l2 = l2.next;
+        //     }
+        // }
+        // return head;
+    }
+}
