e352ee9c24549d7b37519f7360f1750003308ce9
Leon <lkrapinec@gmail.com>
2026-06-17 20:09:48 +0200

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+ 
+        }
+    }
+    
+    public void pop() {
+        int top = stack.pop();
+        if (top == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
