8e4857c885c15400a94d393b4df77720a9591f36
Leon <lkrapinec@gmail.com>
2026-09-02 08:40:23 +0200

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String s) {
+        int first = 1;
+        int second = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int tmp = 0;
+
+            if (s.charAt(i) != '0') {
+                tmp = first;
+                if (i + 1 < s.length()
+                    && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    tmp += second;
+                }
+            }
+
+            second = first;
+            first = tmp;
+        }
+
+        return first;
+    }
+}
